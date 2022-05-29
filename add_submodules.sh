ef12460f0d16085908f2104fc6a2895aeb30c866
FloryanKieffer <kieffer.floryan@gmail.com>
2022-05-29 11:11:16 +0000

    Structure

---
+++ add_submodules.sh
@@ -0,0 +1,2 @@
+git submodule add -b master https://github.com/jluuM2/micro-ecc dechiffrage_component/micro-ecc
+git submodule add -b main https://github.com/jluuM2/Conversion_String_binaire dechiffrage_component/conv-string-bin
