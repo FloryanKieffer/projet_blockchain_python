396d769e9cb970ac0c4d8bb9d48d914d3f2bd677
FloryanKieffer <61381228+FloryanKieffer@users.noreply.github.com>
2022-06-27 09:50:17 +0200

    Add pybind11 in script

--- add_submodules.sh
+++ add_submodules.sh
@@ -1,3 +1,4 @@
 git submodule add -b master https://github.com/jluuM2/micro-ecc chiffrage_component/micro-ecc
 git submodule add -b main https://github.com/jluuM2/Conversion_String_binaire chiffrage_component/conv-string-bin
 git submodule add -b master https://github.com/weidai11/cryptopp chiffrage_component/cryptopp
+git submodule add -b master https://github.com/pybind/pybind11 chiffrage_component/pybind11
